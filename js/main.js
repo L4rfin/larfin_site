b7f7008fbc4292b0ecc98443e3b1907011f35de0
L4rfin <adrian.zajac.it@wp.pl>
2024-05-07 14:42:24 +0200

    link  v0.149.0

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+cube.position.set(0,0,-1)
 scene.add( cube );
 
 camera.position.z = 5;
+
+let customObject;
+const loader = new GLTFLoader();
+loader.load( 'models/znak.gltf', function ( gltf ) {
+  customObject = gltf.scene;
+  scene.add( customObject );
+  customObject.traverse(function (event){
+    console.log(event);
+  });
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
 
 function animate() {
   requestAnimationFrame( animate );
