2ee9d790d7b096a6454063580536413bac3a03c7
L4rfin <adrian.zajac.it@wp.pl>
2024-05-21 19:17:24 +0200

    v0.1.2

--- js/main.js
+++ js/main.js
@@ -3,14 +3,19 @@ import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {RenderPass} from 'three/addons/postprocessing/RenderPass.js';
 import {UnrealBloomPass} from 'three/addons/postprocessing/UnrealBloomPass.js'
 import {EffectComposer} from "three/addons/postprocessing/EffectComposer.js";
+import {Vector3} from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 4;
+camera.position.y = 1;
+camera.aspect = 2;
+
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setPixelRatio(2);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-const  pointLight = new THREE.PointLight( 0xffffff, 1, 100 );
+const pointLight = new THREE.PointLight(0xffffff, 1, 100);
 
 renderer.toneMapping = THREE.CineonToneMapping;
 renderer.toneMappingExposure = 1.5;
@@ -21,96 +26,56 @@ scene.add(pointLight, ambientLight);
 
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
 const cube = new THREE.Mesh(geometry, material);
 cube.position.set(0, 0, -1)
 scene.add(cube);
 
-camera.position.z = 4;
-camera.position.y = 1;
-camera.aspect = 2;
+const sf = new THREE.Mesh(new THREE.SphereGeometry(0.2, 12, 12), new THREE.MeshBasicMaterial({color: 0xff00f0}));
+sf.position.set(0, 0, -10)
+scene.add(sf);
+let listOfBool = [];
 
+console.log(listOfBool);
 //postprocessing
-const renderScene = new RenderPass(scene,camera);
+const renderScene = new RenderPass(scene, camera);
 const composer = new EffectComposer(renderer);
 composer.addPass(renderScene);
 const unrealBloom = new UnrealBloomPass(
-  new THREE.Vector2(window.innerWidth,window.innerHeight),
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
   1.6,
   1,
   0.01
 );
 composer.addPass(unrealBloom);
-
 //shape loader
 const loader = new GLTFLoader();
 
-//rocket
-let rocketSetting = false;
-let rocketToGo = false;
 
-export function GoRocket() {
-  rocketToGo = true
-}
-
-let rocketObject = new THREE.Object3D();
-loader.load('models/rocket.gltf', function (gltf) {
-  rocketObject = gltf.scene;
-  scene.add(rocketObject);
-  rocketObject.traverse(function (event) {
-  });
-}, undefined, function (error) {
-  console.error(error);
-});
-
-function rocketSetSetting() {
-  try {
-    rocketObject.rotation.set(0.0, 0, 0);
-    rocketSetting = true;
-  } catch (exception) {
-    rocketSetting = false;
-  }
-}
-
-function rocketGo() {
-  console.log(rocketObject.position)
-  if (rocketObject.position.x !== 6) {
-    rocketObject.position.x += (rocketObject.position.x > 6) ? -0.1 : 0.1;
-  }
-  if (rocketObject.position.y !== 6) {
-    rocketObject.position.y += (rocketObject.position.y > 6) ? -0.1 : 0.1;
-  }
-  if (rocketObject.position.y >= 5 &&
-    rocketObject.position.y <= 7 &&
-    rocketObject.position.x >= 5 &&
-    rocketObject.position.x <= 7)
-    rocketToGo = false;
-}
-rocketObject.layers.disableAll();
-camera.layers.enableAll();
-
-// rocket end
-
+let angle = 0;
+let radius = 1;
+let speed = 0.1;
+let sfm = 0.2;
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  if (rocketObject) {
-    if (!rocketSetting) {
-      rocketSetSetting();
-    }
-    if (rocketToGo) {
-      rocketGo();
-    }
-
+  if (sf.position.z<=-10.2) {
+    sfm = -sfm;
   }
+  if (sf.position.z>=2.3){
+    sfm = -sfm;
+  }
+  sf.position.z += sfm;
+  sf.position.x = -2 + (radius * Math.sin(angle));
+  sf.position.y = -2 + (radius * Math.cos(angle));
+  angle += speed
+
   requestAnimationFrame(animate);
-  // renderer.render(scene,camera)
   composer.render();
 }
+
 animate();
 
-window.addEventListener('resize', function (){
-  camera.aspect = window.innerWidth/window.innerHeight;
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 })
